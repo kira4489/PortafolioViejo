fe20616c36a9cf59e4791a729c6eeada5739ab02
jhonatanquirama <quiramapino47@gmail.com>
2020-08-26 12:31:13 -0500

    estrcutura de carpetas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Components/Header";
-import Main from "./Components/Main";
-import Footer from "./Components/Footer";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
 function App() {
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import "../Header/Header.css"
+// import styled from 'styled-components'
+//import "../css/Header.css";
+
+// const Navbar = styled.nav`
+// width: 100%;
+// z-index: 999;
+// `
+
+// const Ul = styled.ul`
+// list-style-type: none;
+// overflow: hidden;
+// text-align: center;
+// background-color: black;
+// font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+// `
+
+// const Li = styled.li`
+// display: inline-block;
+// a {
+//   display: inline-block;
+//   color: white;
+//   padding: 25px;
+//   text-decoration: none;
+// }
+// `
+class Header extends Component{
+    render(){
+        return(
+            <header>
+            <nav id="navbar">
+              <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#sobremi">Sobremi</a></li>
+                <li><a href="#projects">Proyectos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+              </ul>
+            </nav>
+          </header>
+        )
+    }
+    
+}
+
+export default Header
