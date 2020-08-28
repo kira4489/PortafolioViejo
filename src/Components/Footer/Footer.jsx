320a3eff26c8c2c20a3bcef8f0e78fc2c9735b65
jhonatanquirama <quiramapino47@gmail.com>
2020-08-28 15:43:46 -0500

    organizandocomponentes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,27 @@ class Footer extends Component{
     render(){
         return(
             <footer>
-                <h1>redesxgdhgg</h1>
-            </footer>
+            <div className="titulo1">
+              <p>© Jhonatan Quirama Pino</p>
+              <div className="redes-footer">
+                <a
+                  href="https://www.facebook.com/jhonatan.quiramapino"
+                  target="_blank"
+                >
+                  <i className="fab fa-facebook-f icon-redes-footer"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/jhonatan-quirama-pino-289096179/"
+                  target="blank"
+                >
+                  <i className="fab fa-linkedin icon-redes-footer"></i>
+                </a>
+                <a href="https://github.com/kira4489">
+                  <i className="fab fa-github icon-redes-footer"></i>
+                </a>
+              </div>
+            </div>
+          </footer>
         )
     }
     
