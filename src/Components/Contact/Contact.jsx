b03aa9a3a83832f48297fb066e076acaa337381b
jhonatanquirama <quiramapino47@gmail.com>
2020-09-08 10:25:35 -0500

    actualizaciones

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,32 +1,51 @@
-import React, { Component } from 'react'
-import "./Contact.css"
+import React, { Component } from "react";
+import "./Contact.css";
 
-class Contact extends Component{
-    render(){
-        return(
+class Contact extends Component {
+  render() {
+    return (
       <section className="formulario">
-      <div className="formulario1" id="contacto">
-        <h2>Vamos a trajar juntos</h2>
-        <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Nombre</label>
-    <input type="text" className="form-control" placeholder="Escriba tu nombre" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="email" className="form-control" placeholder="Escriba tu email" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Mensaje</label>
-    <textarea type="text" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <button type="submit" className="btn btn-primary">Enviar</button>
-</form>
-</div>
-</section>
-        )
-    }
-    
+        <div className="formulario1" id="contacto">
+          <h2>Vamos a trajar juntos</h2>
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Escriba tu nombre"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Escriba tu email"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Mensaje</label>
+              <textarea
+                type="text"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
