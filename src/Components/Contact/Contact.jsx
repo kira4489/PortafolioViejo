5633d8e173f0d365762548def7ffb1232de062b9
jhonatanquirama <quiramapino47@gmail.com>
2020-11-30 11:04:14 -0500

    cambiando color del boton

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -39,7 +39,7 @@ class Contact extends Component {
                 className="form-control"
               />
             </div>
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn">
               Enviar
             </button>
           </form>
