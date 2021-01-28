1b3a6b112a2a4f88de00c975ef4b40fb9d77b0b4
jhonatanquirama <quiramapino47@gmail.com>
2021-01-28 11:56:09 -0500

    eliminando fotos y añadiendo nuevos proyectos

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,43 +3,6 @@ import About from "../About/About";
 import Project from "../Projects/Project";
 import "./Main.css";
 import Contact from "../Contact/Contact";
-// import styled from 'styled-components'
-/**el ultimo elemnto la etiqueta de html */
-// const Section1= styled.section`
-// background-image: url(./portatil.jpg);
-//     height: 100vh;
-//     background-size: cover;
-//     opacity: 0.8;
-//     background-position: center;
-//     position: relative;
-//     ::before {
-//         content: "";
-//         position: absolute;
-//         width: 100%;
-//         height: 100%;
-//         background-color: #000000;
-//         opacity: 0.4;
-//       }
-// `
-
-// const Titulo= styled.section`
-//  position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   h1 {
-//   color: orange;
-//   font-size: 70px;
-//   text-align: center;
-// }
-
-// p {
-//   text-align: center;
-//   font-size: 50px;
-//   color: white;
-// }
-
-// `
 
 class Main extends Component {
   render() {
--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./Project.css";
-import Proyecto1 from "../../images/tributo.jpg";
-import Proyecto2 from "../../images/surveyform.jpg";
-import Proyecto3 from "../../images/landinpage.jpg";
-import Proyecto4 from "../../images/documentpage.jpg";
-import Proyecto5 from "../../images/landing-podcast.jpg";
-import Proyecto6 from "../../images/superhero.jpg";
-import Proyecto7 from "../../images/educationapp.jpg";
-import Proyecto8 from "../../images/burger.jpg";
+import Proyecto1 from "../../images/surveyform.jpg";
+import Proyecto2 from "../../images/documentpage.jpg";
+import Proyecto3 from "../../images/landing-podcast.jpg";
+import Proyecto4 from "../../images/superhero.jpg";
+import Proyecto5 from "../../images/educationapp.jpg";
+import Proyecto6 from "../../images/burger.jpg";
+import Proyecto7 from "../../images/landingpage.jpg";
 import Array from "./Array";
 
 export default class Project extends Component {
@@ -16,44 +15,39 @@ export default class Project extends Component {
     this.state = {
       cards: [
         {
-          titulo: "Tribute page",
-          imagen: Proyecto1,
-          link: "https://blissful-murdock-638985.netlify.app/",
-        },
-        {
           titulo: "Survey form",
-          imagen: Proyecto2,
+          imagen: Proyecto1,
           link: "https://hardcore-goldberg-1a390b.netlify.app/",
         },
         {
-          titulo: "Landing page",
-          imagen: Proyecto3,
-          link: "https://vigorous-dijkstra-2c8cb5.netlify.app/",
-        },
-        {
           titulo: "Document pages",
-          imagen: Proyecto4,
+          imagen: Proyecto2,
           link: "https://cocky-allen-c1d816.netlify.app/",
         },
         {
           titulo: "Landing podcast",
-          imagen: Proyecto5,
+          imagen: Proyecto3,
           link: "https://modest-blackwell-7529ee.netlify.app/",
         },
         {
           titulo: "Super Hero",
-          imagen: Proyecto6,
+          imagen: Proyecto4,
           link: "https://superhero.kira4489.vercel.app/",
         },
         {
           titulo: "Education App",
-          imagen: Proyecto7,
+          imagen: Proyecto5,
           link: "https://final-project-livid.vercel.app/",
         },
         {
           titulo: "Burger Quiz",
-          imagen: Proyecto8,
+          imagen: Proyecto6,
           link: "https://burger-quiz.vercel.app/",
+        },
+        {
+          titulo: "Landing page",
+          imagen: Proyecto7,
+          link: "https://determined-boyd-d73ec1.netlify.app/",
         },
       ]
     } 
