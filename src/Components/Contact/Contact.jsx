987b65125a897c8382a21e4aabb5eb76034d4b4b
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-09-25 13:49:44 -0500

    quit warning and errors

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
           <h2>Vamos a trabajar juntos</h2>
           <form action="https://formspree.io/f/mgenavrv" method="POST">
             <div className="form-group">
-              <label for="exampleInputEmail1">Nombre</label>
+              <label htmlFor="exampleInputEmail1">Nombre</label>
               <input
                 type="text"
                 className="form-control"
@@ -30,7 +30,7 @@ const Contact = () => {
               />
             </div>
             <div className="form-group">
-              <label for="exampleInputPassword3">Mensaje</label>
+              <label htmlFor="exampleInputPassword3">Mensaje</label>
               <textarea
               name="message"
                 type="text"
