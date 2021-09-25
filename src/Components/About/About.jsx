e04fff9f9bec7e88438b17c37dd1e2018422db1c
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-09-25 12:33:02 -0500

    change things about and pdf

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./About.css";
-import foto from "../../images/me.jpg";
+import Me from "../../images/me.jpg";
 import jhonatan from "../../pdf/Jhonatan.pdf";
 
 const About = () =>{
     return (
       <section className="perfil" id="sobremi">
       <div className="container">
-      <img src={foto} class="imagen" />
+      <img src={Me} class="imagen" />
       <div className="texto">
       <p className="title1">Jhonatan Quirama Pino</p>
         <p className="description">
-          Mi nombre es Jhonatan Quirama Pino, tengo 28 años. Egresado
+          Mi nombre es Jhonatan Quirama Pino, tengo 29 años. Egresado
           de academia geek con enfasis en React/Node js. Soy tecnólogo
           en gestión de redes de datos y técnico en Desarrollo de Software, me
           apasiona la informática y la programación, me encantan los
