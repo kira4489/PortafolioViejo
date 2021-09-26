feaed5bdc99f134ce17c76c68926d434e808cf61
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-09-26 17:22:18 -0500

    adding styles responsive

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,29 +3,29 @@ import "./About.css";
 import Me from "../../images/me.jpg";
 import jhonatanQ from "../../pdf/JhonatanQ.pdf";
 
-const About = () =>{
-    return (
-      <section className="perfil" id="sobremi">
+const About = () => {
+  return (
+    <section className="perfil" id="sobremi">
       <div className="container">
-      <img src={Me} className="imagen" alt="jhonatan"/>
-      <div className="texto">
-      <p className="title1">Jhonatan Quirama Pino</p>
-        <p className="description">
-          Mi nombre es Jhonatan Quirama Pino, tengo 29 años. Egresado
-          de academia geek con enfasis en React/Node js. Soy tecnólogo
-          en gestión de redes de datos y técnico en Desarrollo de Software, me
-          apasiona la informática y la programación, me encantan los
-          videojuegos y aprender nuevas cosas todos los días.
-          <a href={jhonatanQ} target="_blank" rel="noopener noreferrer">
-         Ver mi hoja de vida
-        </a>
-        </p>
-        <br />
-        <br />
-       </div>
-       </div>
+        <img src={Me} className="imagen" alt="jhonatan" />
+        <div className="texto">
+          <p className="title1">Jhonatan Quirama Pino</p>
+          <p className="description">
+            Mi nombre es Jhonatan Quirama Pino, tengo 29 años. Egresado de
+            academia geek con enfasis en React/Node js. Soy tecnólogo en gestión
+            de redes de datos y técnico en Desarrollo de Software, me apasiona
+            la informática y la programación, me encantan los videojuegos y
+            aprender nuevas cosas todos los días.
+            <a href={jhonatanQ} target="_blank" rel="noopener noreferrer">
+              Ver mi hoja de vida
+            </a>
+          </p>
+          <br />
+          <br />
+        </div>
+      </div>
     </section>
-    );
-}
+  );
+};
 
 export default About;
