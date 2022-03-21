3fda224a8cc94f06edce1a18bee4fc1c74543ecc
Jhonatan Quirama Pino <jhonatan.quirama.pino@kyndryl.com>
2022-03-21 17:15:45 -0500

    adding new section job

---
+++ src/Components/Experience/Experience2.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Experience2.css";
+
+const Experience2 = () => {
+    return (
+        <div className="container2-experience">
+            <div className="container2-title" id="experience">
+                <h2>Middleware Junior</h2>
+                <ul>Soporte de capa media tales como:
+                    <li>Mq,datastage,sterling</li>
+                    <li>Soporte en aws,configuracion de pipelines para reinicio de mq o actualizaciones</li>
+                </ul>
+                <p className="paragraph2-company">
+                    <strong>Empresa:</strong> Db system
+                </p>
+                <p>
+                    <strong>Periodo:</strong> 16 de diciembre 2021 - Actualidad
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Experience2
